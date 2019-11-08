a7e871bedbdef0ec4e05009d69a8b59593a3a785
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2019-11-08 15:42:43 -0300

    Fixing functions
    Co-authored-by: Micaella <micaella2212@gmail.com>

--- trab2.asm
+++ trab2.asm
@@ -1,153 +1,134 @@
 .data
 ask_str1: .asciiz "Multiplicando: "
 ask_str2: .asciiz "Multiplicador: "
-result_str: .asciiz ""
-.align 2
+ask_str3: .asciiz "Hi: "
+ask_str4: .asciiz "Lo: "
 
-.text
-.globl tart
+.text 
 
-main: 
+main:
 
-# $a1 -> multiplicando
-# $a2 -> multiplicador
-# $v1 -> multiplicar por -1
-# $a2 -> end base do vetor que armazena os bits do multiplicando
-# $a3 -> end base do vetor que armazena os bits do multiplicador
+# ------------- TRABALHO 2 - FAC - 2/2019 ---------------------
+#             Micaella Gouveia - 17/0111288
+#             Sofia Patrocínio - 17/0114333
 
-addi $v1, $zero, -1
-add $a2, $zero, $zero
 
-start:
+# Variáveis
+# $s0 -> multiplicando
+# $s1 -> multiplicador
+# $s2 -> n = 32
+# $s3 -> HI
+# $s4 -> LO
+# $a1 -> Hi definitivo
+# $a2 -> Lo definitivo
+# $t0 -> Hi temporário
+# $t1 -> Lo temporário
+# $t2 -> i
+# $t3 -> Bit extra
+# $t4 -> último bit do Lo
+# $t5 -> verificar se deve fazer a operação
+# $t6 -> Hi +- Multiplicando
+# $t7 -> último bit de Hi
+
+add $t3, $zero, $zero  # bit extra = 0
+add $t2, $zero, $zero  # i = 0
+addi $s2, $zero, 32  # n = 32
+
 # Ler o multiplicando
 li $v0, 4
 la $a0, ask_str1
 syscall
+
 li $v0, 5
 syscall
-move $a1, $v0
+move $s0, $v0  # $s0 = Multiplicando
 
 # Ler o multiplicador
 li $v0, 4
 la $a0, ask_str2
 syscall
+
 li $v0, 5
 syscall
-move $a2, $v0
-#------------------------------------------------------------
+move $s1, $v0  # $s1 = Multiplicador
 
-slt $t5, $a1, $zero  # 1 -> multiplicando < 0
-bne $t5, $zero, mult1
-j continua
+jal Multifac
+j exit
 
-mult1:
+Multifac:
 
-mul $a1, $a1, $v1  # Multiplicamos o número por -1 (fica positivo)
-j continua
+add $t0, $zero, $zero  # Zerar Hi
 
-continua:
-
-slt $t5, $a2, $zero  # 1 -> multiplicador < 0
-bne $t5, $zero, mult2
-j conversor
-
-mult2:
-
-mul  $a2, $a2, $v1  # Multiplicamos o número por -1 (fica positivo)
-j conversor
-
-#--------------------------------------------------------------
-conversor:
-
-# Convertendo o multiplicando
-
-add $t0, $zero, $a1 # put our input ($a1) into $t0
-add $t1, $zero, $zero # Zero out $t1
-addi $t3, $zero, 1 # load 1 as a mask
-sll $t3, $t3, 31 # move the mask to appropriate position
-addi $t4, $zero, 32 # loop counter
+add $t1, $zero, $s1  # Multiplicador em Lo
 
 loop:
 
-and $t1, $t0, $t3 # and the input with the mask
-beq $t1, $zero, print # Branch to print if its 0
+beq $t2, $s2, exit      # Condição de Parada
 
-add $t1, $zero, $zero # Zero out $t1
-addi $t1, $zero, 1 # Put a 1 in $t1
+andi $t4, $t1, 1        #   1 -> multiplicador terminar com 1
+                        #   0 -> multiplicador terminar com 0
 
-j print
 
-print: 
+xor $t5, $t4, $t3    # $t5 = ultimo bit de lo XOR bit extra
 
-li $v0, 1
-move $a0, $t1
+bne $t5, $zero, operacao  # Se o último bit de Lo e o bit extra forem diferentes, ir para a operação 
+j shift                   # Se foram iguais, nada a fazer, então vamos para o shift lógico
+
+operacao:
+
+beq $t3, $zero, subtrair  # Se o bit extra é 0, então temos "10" -> subtraímos
+add $t0, $t0, $s0  # Se não, Hi = Hi + Multiplicando
+j shift
+
+subtrair:
+sub $t0, $t0, $s0  # Hi = Hi - Multiplicando
+j shift
+
+shift:
+
+# Shift Hi
+andi $t7, $t0, 1  # $t7 = último bit do Hi
+sra $t0, $t0, 1   # shift aritmético no Hi
+
+# Shift Lo
+add $t3, $t4, $zero  # Passa o último bit de Lo para o Bit Extra
+srl $t1, $t1, 1      # shift no Lo
+
+sll $t7, $t7, 31     # Ultimo bit de Hi
+add $t1, $t1, $t7    # Passar ultimo bit de Hi para o primeiro de Lo
+
+addi $t2, $t2, 1  # i++
+j loop
+
+jr $ra
+
+exit:
+
+mthi $t0
+mtlo $t1
+
+mfhi $s3
+mflo $s4
+
+# Printar o Hi
+li $v0, 4
+la $a0, ask_str3
 syscall
 
-srl $t3, $t3, 1
-addi $t4, $t4, -1
-bne $t4, $zero, loop
-
-#jr $ra
+li $v0, 1
+move $a0, $s3
+syscall
 
 # New Line
 li $v0, 11
 li $a0, 10
 syscall
 
-
-# Convertendo o multiplicador
-
-add $t0, $zero, $a2 # put our input ($a2) into $t0
-add $t6, $zero, $zero # Zero out $t6
-addi $t3, $zero, 1 # load 1 as a mask
-sll $t3, $t3, 31 # move the mask to appropriate position
-addi $t4, $zero, 32 # loop counter
-
-loop1:
-
-and $t6, $t0, $t3 # and the input with the mask
-beq $t6, $zero, print1 # Branch to print if its 0
-
-add $t6, $zero, $zero # Zero out $t6
-addi $t6, $zero, 1 # Put a 1 in $t6
-j print1
-
-print1: li $v0, 1
-move $a0, $t6
+# Printar o Lo
+li $v0, 4
+la $a0, ask_str4
 syscall
 
-srl $t3, $t3, 1
-addi $t4, $t4, -1
-bne $t4, $zero, loop1
-
-jr $ra
-
-
-
-
-
-
-
-
-
-
-#li $v0, 1
-#move $a0, $a1
-#syscall
-
-#li $v0, 11
-#li $a0, 10
-#syscall
-
-#li $v0, 1
-#move $a0, $a2
-#syscall
-
-#jal conversor
-
-# New Line
-#li $v0, 11
-#li $a0, 10
-#syscall
-
-#j start
+li $v0, 1
+move $a0, $s4
+syscall
